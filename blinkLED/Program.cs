a9ee2c9d845676da2d57812b9873f9824e0a2958
007roy <troy.bennett@gmail.com>
2021-03-08 14:19:52 -0800

    blinkLed and toggleled

---
+++ blinkLED/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+
+namespace blinkLED
+{
+    class Program
+    {
+        private const int LEDPin = 17;
+        private static CancellationTokenSource cts = new CancellationTokenSource();
+        private static CancellationToken ct = cts.Token;
+        private static GpioController controller = new GpioController();
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(BlinkLight,ct);
+            Console.ReadKey();
+            cts.Cancel();
+            controller.Dispose();
+        }
+        static void BlinkLight()
+        {
+            controller.OpenPin(LEDPin, PinMode.Output);
+            PinValue pinState = PinValue.High;
+            while(!ct.IsCancellationRequested){
+                controller.Write(LEDPin, pinState);
+                pinState = pinState.Equals(PinValue.High) ? PinValue.Low : PinValue.High;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ toggleSwitchLED/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace toggleSwitchLED
+{
+    class Program
+    {
+        private static GpioController controller = new GpioController();
+        private static bool LightOn = false;
+        private static int lastChangeTime = 0;
+        static void Main(string[] args)
+        {
+
+            controller.OpenPin(17, PinMode.Output);
+            controller.OpenPin(18, PinMode.InputPullUp);
+            controller.RegisterCallbackForPinValueChangedEvent(18, PinEventTypes.Rising | PinEventTypes.Falling, OnPinChange);
+            Console.ReadKey();
+        }
+        private static void OnPinChange(object sender, PinValueChangedEventArgs args)
+        {
+            if (DateTime.Now.Millisecond - lastChangeTime > 50)
+            {
+                if (args.ChangeType == PinEventTypes.Rising)
+                {
+                    ToggleLight();
+                }
+            }
+            lastChangeTime = DateTime.Now.Millisecond;
+        }
+        static void ToggleLight()
+        {
+            LightOn = LightOn ? false : true;
+            if (LightOn) { controller.Write(17, PinValue.High); }
+            else { controller.Write(17, PinValue.Low); }
+        }
+    }
+}
